f9ca6fa26c3534b5de8d4ea2902bfb5080d95fdb
Vinicius <viniciusdub8@gmail.com>
2026-09-07 15:49:35 -0300

    Adicionando arquivos da atividade3

--- praticas/atividade3/questao6.py
+++ praticas/atividade3/questao6.py
@@ -5,7 +5,7 @@ senha_digitada = input("Digite sua senha: ")
 acesso_liberado = senha_cadastrada == senha_digitada
 print("Acesso liberado?", acesso_liberado)
 
-"""
+""" Resposta Escrita
 Isso acontece devido à diferença entre os tipos de dados no Python
 
 A variável senha_cadastrada = 1234 foi armazenada como um número inteiro.
