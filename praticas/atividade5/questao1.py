02a8a9c12664f8632a51cd81b6cd4de8e0c91311
Vinicius <viniciusdub8@gmail.com>
2026-09-07 13:55:10 -0300

    Atualizando nomes de arquivos para padrao

---
+++ praticas/atividade5/questao1.py
@@ -0,0 +1,20 @@
+print("Código |Produto         |Preço "
+      "\n  1    |Cachorro-quente |R$ 10,00 "
+      "\n  2    |Hambúrguer      |R$ 15,00 "
+      "\n  3    |Batata Frita    |R$  8,00 "
+      "\n  4    |Refrigerante    |R$  5,00 \n")
+
+id_produto = input("Digite o ID do produto que você deseja: ")
+
+match id_produto:
+    case '1':
+        print("Cachorro-quente | R$ 10,00 ")
+    case '2':
+        print("Hambúrguer | R$ 15,00 ")
+    case '3':
+        print("Batata Frita | R$ 8,00 ")
+    case '4':
+        print("Refrigerante | R$ 5,00 ")
+    case _:
+        print("Código inválido.")
+
