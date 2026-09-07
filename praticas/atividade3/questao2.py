c5072391fe55f52fef3bb7861266cb913c87cdec
Vinicius <viniciusdub8@gmail.com>
2026-09-07 15:17:59 -0300

    Atualizando arquivo

--- praticas/atividade3/questao2.py
+++ praticas/atividade3/questao2.py
@@ -1,9 +1,8 @@
-print("Questão 2: A Fábrica de Caixas (Operador de Módulo) \n");
+print("Questão 2: A Fábrica de Caixas (Operador de Módulo)\n")
 
-total_macas = int(input("Digite a quantidade total de maçãs "
-                        "colhidas no dia: "))
+total_macas = int(input("Digite a quantidade total de maçãs colhidas no dia: "))
 sobras = total_macas % 12
 caixas = total_macas // 12
 
-print("Sobrarão", sobras,"maçãs fora das caixas.")
-print(caixas)
+print("Sobrarão", sobras, "maçãs fora das caixas.")
+print("Total de caixas completas:", caixas)
