6d46626e662f4c70ec7b0c0a2dbd7c4677a5e247
Vinicius <viniciusdub8@gmail.com>
2026-09-16 21:17:48 -0300

    Adicionando arquivo da atividade1_poo

--- praticas/atividades_poo/atividade1/Carro.py
+++ praticas/atividades_poo/atividade1/Carro.py
@@ -1,29 +1,61 @@
 class Carro:
-    def __init__(self, marca, ano, nome, modelo):
+    def __init__(self, marca, ano, nome, modelo, cor):
         self.marca = marca
         self.ano = ano
         self.nome = nome
         self.modelo = modelo
-
-    def mostrar(self):
-        print(self.marca, self.ano, self.nome, self.modelo)
+        self.cor = cor
 
     def __str__(self):
-        return f"Marca: {self.marca}\nAno: {self.ano}\nNome: {self.nome}\nModelo: {self.modelo}"
+        return (f"Informações do Veiculo:\n\t"
+                f"Marca: {self.marca}\n\t"
+                f"Ano: {self.ano}\n\t"
+                f"Nome: {self.nome}\n\t"
+                f"Modelo: {self.modelo}\n\t"
+                f"Cor: {self.cor}\n")
+
+    def ligar(self):
+        return print(f"O {self.nome} está Ligado!")
+
+    def desligar(self):
+        print(f"O {self.nome} está Desligado!")
+
+    def acelerar(self):
+        print(f"O {self.nome} está Acelerando!")
+
 
 carro1 = Carro("Volkswagen",
                2013,
                "Golf",
-               "Hatch")
+               "Hatch",
+               "Preto")
 
 carro2 = Carro("Honda",
                2025,
                "Civic",
-               "Sedan")
+               "Sedan",
+               "Vermelho")
 
 carro3 = Carro("Toyota",
                2020,
                "Hillux SW4",
-               "SUV")
+               "SUV",
+               "Branca")
 
-carro1.mostrar()
+carro4 = Carro("Nissan",
+               1999,
+               "Skyline R34",
+               "Sedan",
+               "Azul Escuro")
+
+carro5 = Carro("BYD",
+               2025,
+               "Seal",
+               "Sedan",
+               "Azul marinho")
+
+
+carros = [carro1, carro2, carro3, carro4, carro5]
+
+for exibir_carros in carros:
+    print(exibir_carros)
