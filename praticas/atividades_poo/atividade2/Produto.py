92605326ed14f3052881aea34da25b2740174946
Vinicius <viniciusdub8@gmail.com>
2026-09-18 20:30:31 -0300

    Adicionando arquivo da atividade2_poo

--- praticas/atividades_poo/atividade2/Produto.py
+++ praticas/atividades_poo/atividade2/Produto.py
@@ -5,29 +5,63 @@ class Produto:
         self.__quantidade_estoque = quantidade_estoque
 
     def adicionar_estoque(self, quantidade):
-        quantidade = int(input("Quantidade adicionada no estoque: "))
         if quantidade > 0:
+            print("METODO: ADICIONAR_ESTOQUE")
+            print(f"Quantidade no estoque: {self.__quantidade_estoque}")
             print(f"Quantidade adicionada: {quantidade}")
             self.__quantidade_estoque += quantidade
-            print(f"Estoque Atualizado: {self.__quantidade_estoque}")
+            print(f"Estoque Atualizado: {self.__quantidade_estoque}\n")
         else:
-            print("Erro: Quantidade inválida")
+            print("Erro: Quantidade inválida\n")
 
-    # def realizar_venda(self, quantidade):
-    #     quantidade = int(input("Quantidade retirada do estoque: "))
-    #     if quantidade <= self.__quantidade_estoque:
-    #         self.__quantidade_estoque -= quantidade
-    #         print(f"Quantidade retirada: {quantidade}")
-    #         print(f"Venda aprovada!")
-    #     else:
-    #         print(f"Venda negada. Estoque insuficiente")
-    #
-    # def aplicar_desconto(self, percentual):
-    #     percentual = int(input("Percentual de desconto: "))
-    #     if percentual <= 80:
-    #         desconto = self.__preco * (percentual / 100)
-    #         print(f"Valor atual: {self.__preco}")
-    #         print(f"Desconto aplicado: {desconto}")
+    def realizar_venda(self, quantidade):
+        if quantidade <= self.__quantidade_estoque:
+            print("METODO: REALIZAR_VENDA")
+            print(f"Quantidade no estoque: {self.__quantidade_estoque}")
+            self.__quantidade_estoque -= quantidade
+            print(f"Quantidade vendida: {quantidade}")
+            print(f"Venda aprovada!\n")
+        else:
+            print(f"Venda negada. Estoque insuficiente\n")
 
-print("testando")
+    def aplicar_desconto(self, percentual):
+        if percentual <= 80:
+            print("METODO: APLICAR_DESCONTO")
+            self.__preco = self.__preco * (percentual / 100)
+            print(f"{self.__nome} com {percentual}% de desconto por apenas R${self.__preco:.2f}\n")
 
+        else:
+            print(f"Erro: Desconto inválido\n")
+
+    def exibir_resumo(self):
+        return print(f"METODO: EXIBIR_RESUMO\n"
+                f"Nome: {self.__nome}\n"
+                f"Preço: R$ {self.__preco}\n"
+                f"Qtd. Estoque: {self.__quantidade_estoque}\n")
+
+novo_produto = Produto("Coca Cola 2 Litros", 15.00, 50)
+
+
+# Testando metodo adicionar_estoque;
+novo_produto.adicionar_estoque(100)
+novo_produto.adicionar_estoque(-100)
+# Testando metodo aplicar_desconto;
+novo_produto.aplicar_desconto(50)
+novo_produto.aplicar_desconto(90)
+# Testando metodo adicionar_estoque;
+
+
+# 1. Tente forçar a alteração direta dos atributos
+# (O Python permite criar variável fora, mas não altera a original):
+novo_produto.__quantidade_estoque = -99
+novo_produto.__preco = -999
+
+# 2. Tente realizar uma venda absurdamente maior do que o estoque que
+# você cadastrou inicialmente:
+novo_produto.realizar_venda(25)
+novo_produto.realizar_venda(200)
+
+# 3. Exiba o resumo final. O estoque e o preço reais não podem
+# ter sido afetados pelos testes maliciosos acima!
+novo_produto.exibir_resumo()
+print(novo_produto.__dict__)
