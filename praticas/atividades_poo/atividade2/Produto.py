99b43d026df68e348bcbdd32c25f07f27daa1864
Vinicius <viniciusdub8@gmail.com>
2026-09-18 21:21:16 -0300

    Adicionando arquivo da atividade2_poo

--- praticas/atividades_poo/atividade2/Produto.py
+++ praticas/atividades_poo/atividade2/Produto.py
@@ -27,9 +27,9 @@ class Produto:
     def aplicar_desconto(self, percentual):
         if percentual <= 80:
             print("METODO: APLICAR_DESCONTO")
-            self.__preco = self.__preco * (percentual / 100)
-            print(f"{self.__nome} com {percentual}% de desconto por apenas R${self.__preco:.2f}\n")
-
+            desconto = self.__preco * (percentual / 100)
+            preco_novo = self.__preco - desconto
+            print(f"{self.__nome} com {percentual}% de desconto por apenas R${preco_novo:.2f}\n")
         else:
             print(f"Erro: Desconto inválido\n")
 
@@ -39,29 +39,33 @@ class Produto:
                 f"Preço: R$ {self.__preco}\n"
                 f"Qtd. Estoque: {self.__quantidade_estoque}\n")
 
-novo_produto = Produto("Coca Cola 2 Litros", 15.00, 50)
+novo_produto = Produto("RTX 5060 Ti", 4500.00, 20)
 
-
-# Testando metodo adicionar_estoque;
-novo_produto.adicionar_estoque(100)
-novo_produto.adicionar_estoque(-100)
-# Testando metodo aplicar_desconto;
-novo_produto.aplicar_desconto(50)
-novo_produto.aplicar_desconto(90)
-# Testando metodo adicionar_estoque;
-
-
-# 1. Tente forçar a alteração direta dos atributos
-# (O Python permite criar variável fora, mas não altera a original):
+""" 
+1. Tente forçar a alteração direta dos atributos
+ (O Python permite criar variável fora, mas não altera a original): """
 novo_produto.__quantidade_estoque = -99
 novo_produto.__preco = -999
 
-# 2. Tente realizar uma venda absurdamente maior do que o estoque que
-# você cadastrou inicialmente:
-novo_produto.realizar_venda(25)
-novo_produto.realizar_venda(200)
+"""
+2. Tente realizar uma venda absurdamente maior do que o estoque que
+você cadastrou inicialmente: """
+novo_produto.realizar_venda(30)
 
-# 3. Exiba o resumo final. O estoque e o preço reais não podem
-# ter sido afetados pelos testes maliciosos acima!
+"""
+3. Exiba o resumo final. O estoque e o preço reais não podem
+ter sido afetados pelos testes maliciosos acima! """
 novo_produto.exibir_resumo()
 print(novo_produto.__dict__)
+
+
+# Testando metodo adicionar_estoque
+# novo_produto.adicionar_estoque(15)
+# novo_produto.adicionar_estoque(-100)
+#
+# Testando metodo aplicar_desconto
+# novo_produto.aplicar_desconto(15)
+# novo_produto.aplicar_desconto(90)
+#
+# Testando metodo realizar_venda
+# novo_produto.realizar_venda(20)
